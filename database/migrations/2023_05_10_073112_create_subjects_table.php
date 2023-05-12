62189f08f34cef89645690a0beaf42c89186e5ed
Loi <lytatloizz.no1@gmail.com>
2023-05-12 21:00:46 +0700

    Update core(layout:login + register) function dashbroad + login + register + signout + timetable

---
+++ database/migrations/2023_05_10_073112_create_subjects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->id('subject_id');
+            $table->string('subject_name');
+            $table->string('subject_description')->nullable();
+            $table->integer('subject_numbers');
+            $table->time('start_at');
+            $table->time('end_at');
+            $table->integer('week_day');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('classroom_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('classroom_id')->references('classroom_id')->on('classrooms');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+};
